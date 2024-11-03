87972b1c7cc909080324fa1e9c7f74fb8b2a6195
Gian Michele Innocenti <ginnocen@cern.ch>
2024-11-03 16:08:48 +0100

    update samples

--- SampleGeneration/20241027_ForestReducer_DzeroUPC/RunExamples.sh
+++ SampleGeneration/20241027_ForestReducer_DzeroUPC/RunExamples.sh
@@ -7,13 +7,13 @@ mkdir -p Output/
 ### Data PbPb ###
 #################
 
-#./Execute --Input /eos/cms/store/group/phys_heavyions/jdlang/PbPbUPC2023/2023config_2023RerecoHIForward0/HIForward0/crab_2023config_2023RerecoHIForward0/241024_101446/0001/HiForestMiniAOD_1703.root \
-./Execute --Input ../../SamplesForest23HIVertex/HiForestMiniAOD_UPCPbPb23_HiVertex_279.root \
+./Execute --Input /data/NewSkims23_24/20241102_ForestOldReco23sample_Dataexample/HiForestMiniAOD_UPCPbPb23_HiVertex_279.root \
    --Output Output/example.root \
    --Year 2023 \
    --MinDzeroPT 1.0 \
    --IsData true \
    --PFTree particleFlowAnalyser/pftree \
+
 ###################
 ### MC Gen PbPb ###
 ###################
@@ -25,5 +25,3 @@ mkdir -p Output/
    --IsData false \
    --PFTree particleFlowAnalyser/pftree \
    --DGenTree Dfinder/ntGen
-
-
--- SampleGeneration/20241027_ForestReducer_DzeroUPC/RunParallel.sh
+++ SampleGeneration/20241027_ForestReducer_DzeroUPC/RunParallel.sh
@@ -3,31 +3,9 @@
 OUTPUT="output"
 counter=0
 MAXCORES=100
-filelist="/home/ginnocen/MITHIGAnalysis2024/sparecode/output.txt"
-MERGEDOUTPUT="MergedOutput.root"
+filelist="/data/NewSkims23_24/20241102_SkimOldReco23sample_DataAllPDs/filelist_short.txt"
+MERGEDOUTPUT="MergedSkim_20241102_SkimOldReco23sample_DataAllPDs.root"
 rm $MERGEDOUTPUT
-
-# Check the number of physical CPUs
-#NUM_PHYSICAL_CPUS=$(sysctl -n hw.physicalcpu)
-
-## Verify that MAXCORES is not larger than the number of physical CPUs
-#if (( MAXCORES > NUM_PHYSICAL_CPUS )); then
-#    echo "Error: MAXCORES ($MAXCORES) exceeds the number of physical CPUs ($NUM_PHYSICAL_CPUS)."
-#    exit 1
-#fi
-
-# Remove the file if it exists, suppress error if it doesn't
-#rm -f "$filelist"
-
-# Create the output directory if it doesn't exist
-mkdir -p "$OUTPUT"
-
-# List all matching files and write to the filelist
-
-#ls  /eos/cms/store/group/phys_heavyions/ginnocen/PbPb2018_gtoccbar/20241023_DiJetpThat15PbPb2018gtoccbar_v1/DiJet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/HINPbPbSpring21MiniAOD-FixL1CaloGT_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM/DiJet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/20241023_DiJetpThat15PbPb2018gtoccbar_v1/241023_225656/000*/*.root > "$filelist"
-#ls /eos/cms/store/group/phys_heavyions/jdlang/PbPbUPC2023/2023config_2023RerecoHIForward0/HIForward0/crab_2023config_2023RerecoHIForward0/241024_101446/000*/*.root > "$filelist"
-#ls /eos/cms/store/group/phys_heavyions/jdlang/run3MC/Run3UPC2024_Rereco24HiVtx_MC_pthat0_gammaN_100M/PYTHIA8_2024Run3/crab_Run3UPC2024_Rereco24HiVtx_MC_pthat0_gammaN_100M/241031_225819/0000/* > "$filelist"
-#ls Samples/HiForestAOD_*_Data_pp.root > "$filelist"
 
 # Check if the filelist is empty
 if [[ ! -s "$filelist" ]]; then
@@ -45,8 +23,9 @@ while IFS= read -r file; do
             --Output "$OUTPUT/output_$counter.root" \
             --Year 2023 \
             --IsData true \
-            --MinDzeroPT 2.0 \
+            --MinDzeroPT 1.0 \
             --PFTree particleFlowAnalyser/pftree &
+ #           --DGenTree Dfinder/ntGen &
     ((counter++))
     if (( counter % $MAXCORES == 0 )); then
         wait
@@ -57,4 +36,41 @@ wait
 hadd $MERGEDOUTPUT $OUTPUT/output_*.root
 echo "All done!"
 echo "Merged output file: $MERGEDOUTPUT"
+
+OUTPUTMC="output"
+counterMC=0
+MAXCORESMC=100
+filelistMC="/data/NewSkims23_24/20241102_SkimOldReco23sample_MCPthat2/filelist.txt"
+MERGEDOUTPUTMC="MergedSkim_20241102_SkimOldReco23sample_MCPthat2.root"
+rm $MERGEDOUTPUTMC
+
+# Check if the filelist is empty
+if [[ ! -s "$filelistMC" ]]; then
+    echo "No matching files found in Samples directory."
+    exit 1
+fi
+
+echo "File list created successfully: $filelistMC"
+rm -rf $OUTPUTMC
+mkdir $OUTPUTMC	
+# Loop through each file in the file list
+while IFS= read -r file; do
+            echo "Processing $file"
+            ./Execute --Input "$fileMC" \
+            --Output "$OUTPUTMC/output_$counterMC.root" \
+            --Year 2023 \
+            --IsData false \
+            --MinDzeroPT 1.0 \
+            --PFTree particleFlowAnalyser/pftree &
+            --DGenTree Dfinder/ntGen &
+    ((counterMC++))
+    if (( counterMC % $MAXCORES == 0 )); then
+        wait
+    fi
+done < "$filelistMC"
+wait 
+
+hadd $MERGEDOUTPUTMC $OUTPUTMC/output_*.root
+echo "All done MC!"
+echo "Merged output file: $MERGEDOUTPUTMC"
 
