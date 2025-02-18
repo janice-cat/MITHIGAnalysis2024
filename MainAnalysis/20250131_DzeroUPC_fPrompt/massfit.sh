ef1f107a7cfc8b46b83c0ae9b26a0fd5c9728070
Janice Chen <janice.chen45@gmail.com>
2025-02-18 20:50:01 +0100

    [merge] the peaking background development on the main branch

--- MainAnalysis/20250131_DzeroUPC_fPrompt/massfit.sh
+++ MainAnalysis/20250131_DzeroUPC_fPrompt/massfit.sh
@@ -19,8 +19,13 @@ jq -c '.MicroTrees[]' $FitSettingCard | while read MicroTree; do
 	effmcInput=$(echo $MicroTree | jq -r '.effmcInput')
 	doSyst_sig=$(echo $MicroTree | jq -r '.doSyst_sig')
 	doSyst_comb=$(echo $MicroTree | jq -r '.doSyst_comb')
-	RstDir=$(dirname "$dataInput")
-	RstDir=${RstDir}/${FitDir}/
+	doPkkk=$(echo $MicroTree | jq -r '.doPkkk')
+	doPkpp=$(echo $MicroTree | jq -r '.doPkpp')
+	RstDir=$(echo $MicroTree | jq -r '.RstDir')
+	if [[ $RstDir == "null" ]]; then
+		RstDir=$(dirname "$dataInput")
+		RstDir=${RstDir}/${FitDir}/
+	fi
 	mkdir -p $RstDir
 	if [[ $FitSettingCard == *"fitSettings/"* ]]; then
 		# Create a new JSON with FitDir and the current MicroTree
@@ -39,6 +44,8 @@ jq -c '.MicroTrees[]' $FitSettingCard | while read MicroTree; do
   [ "$neventsInput" != "null" ] && cmd="$cmd --neventsInput $neventsInput"
   [ "$doSyst_sig" != "null" ] && cmd="$cmd --doSyst_sig $doSyst_sig"
   [ "$doSyst_comb" != "null" ] && cmd="$cmd --doSyst_comb $doSyst_comb"
+  [ "$doPkkk" != "null" ] && cmd="$cmd --doPkkk $doPkkk"
+  [ "$doPkpp" != "null" ] && cmd="$cmd --doPkpp $doPkpp"
   cmd="$cmd --Output fit.root --RstDir $RstDir"
 
   echo "Executing >>>>>>"
@@ -49,15 +56,16 @@ jq -c '.MicroTrees[]' $FitSettingCard | while read MicroTree; do
   # Execute the command and log output
   $cmd >> $RstDir/fit.log
 
-  echo '================================================' >> $RstDir/fit.log
-  echo '= Getting corrected yields:' >> $RstDir/fit.log
-  echo '================================================' >> $RstDir/fit.log
-  cmd="./CorrectedYields --rawYieldInput $RstDir/fit.root --effInput $effmcInput --Output $RstDir/correctedYields.md"
+  ##### DEACTIVATED CORRECTEDYIELDS PROCEDURE
+  # echo '================================================' >> $RstDir/fit.log
+  # echo '= Getting corrected yields:' >> $RstDir/fit.log
+  # echo '================================================' >> $RstDir/fit.log
+  # cmd="./CorrectedYields --rawYieldInput $RstDir/fit.root --effInput $effmcInput --Output $RstDir/correctedYields.md"
 
-  echo "Executing >>>>>>"
-  echo $cmd
-  echo "Executing >>>>>>" >> $RstDir/fit.log
-  echo $cmd >> $RstDir/fit.log
+  # echo "Executing >>>>>>"
+  # echo $cmd
+  # echo "Executing >>>>>>" >> $RstDir/fit.log
+  # echo $cmd >> $RstDir/fit.log
 
-  $cmd >> $RstDir/fit.log
+  # $cmd >> $RstDir/fit.log
 done
